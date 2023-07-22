93e2540f280471aa66895526ef9bdb24b1eb9153
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-07-23 00:26:59 +0500

    Added new tasks program in the practical phase 2

--- practical_phase2/task3.py
+++ practical_phase2/task3.py
@@ -1,3 +1,4 @@
+#filter out files and make the output in upper case
 myfiles = ["abd.txt", "abc.csv", "test.txt", "ali.txt", "abdeali.csv"]
-updated_iles=[i.upper() for i in myfiles if i!="abdeali.csv"]
-print(updated_iles)
+updated_files=[i.upper() for i in myfiles if i!="abdeali.csv"]
+print(updated_files)
