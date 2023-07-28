df7a4d91a7f3e6e31fd53675f52ea12f3123b0a9
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-07-28 18:38:30 +0500

    update the convert datetime in the form of seconds into human readabale form.py program, as we adding datetime module method also

--- Time Module/covert datetime in the form of seconds into human readabale form.py
+++ Time Module/covert datetime in the form of seconds into human readabale form.py
@@ -1,6 +1,14 @@
+from datetime import datetime
 import time
 current_time=time.time()
-print(current_time)
-updated_current_time=time.ctime(current_time)
-print(updated_current_time)
-print(type(updated_current_time))
+print(current_time) #show date and time in seconds
+
+#1st method using time module
+updated_current_time1=time.ctime(current_time)
+print(updated_current_time1)
+print(type(updated_current_time1)) # type is string
+
+#seconds method using datetime module
+updated_current_time2=datetime.fromtimestamp(current_time)
+print(updated_current_time2)
+print(type(updated_current_time2)) #type is datetime object
