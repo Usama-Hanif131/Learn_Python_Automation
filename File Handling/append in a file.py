1bf137b477567ba3b4ec9b51d0d3a5aba124b52b
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-07-30 09:02:12 +0500

    add the read file code

--- File Handling/append in a file.py
+++ File Handling/append in a file.py
@@ -1,2 +1,6 @@
 with open("E:\\Knowledge_Hub\\Devops\\Python_Automation\\Notes\\Practice\\file1.txt", "a") as f:
-    f.write("we we apped omething")
+    f.write("\nHere we append something")
+f.close()
+with open("E:\\Knowledge_Hub\\Devops\\Python_Automation\\Notes\\Practice\\file1.txt", "r") as g:
+    print(g.read())
+g.close()
