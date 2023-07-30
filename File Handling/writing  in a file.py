1bf1b01a37fa9655204c19a8d60627bc3a0c8aed
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-07-30 08:42:23 +0500

    add the reading file code also and adding comments

--- File Handling/writing  in a file.py
+++ File Handling/writing  in a file.py
@@ -1,3 +1,8 @@
+#writing in a file
 with open("E:\\Knowledge_Hub\\Devops\\Python_Automation\\Notes\\Practice\\file1.txt","w") as f:
     f.write("Hallo, ich bin Osama!")
 f.close()
+#reading file
+with open("E:\\Knowledge_Hub\\Devops\\Python_Automation\\Notes\\Practice\\file1.txt","r") as g:
+    print(g.read())
+g.close()
