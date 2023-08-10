0a62128ceda4eec2458b682a77c647767021e4e9
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-08-10 15:55:03 +0500

    adding comments in date and time program

--- Date and Time Module/data and time.py
+++ Date and Time Module/data and time.py
@@ -1,6 +1,6 @@
-from datetime import datetime
-current_time=datetime.now()
-print(current_time)
-print(current_time.day)
-print(current_time.month)
-print(current_time.year)
+from datetime import datetime  # Import the 'datetime' module from the 'datetime' package
+current_time = datetime.now()  # Get the current date and time
+print("Current time:", current_time)  # Print the current date and time
+print("Day:", current_time.day)  # Print the day of the month from the current date and time
+print("Month:", current_time.month)  # Print the month from the current date and time
+print("Year:", current_time.year)  # Print the year from the current date and time
