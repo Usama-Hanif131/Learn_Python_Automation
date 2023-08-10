2efb37a3f68826ee6463632c92d65428616d8eb5
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-08-10 16:00:05 +0500

    adding the comments

--- Date and Time Module/use of pytz for setting timezone.py
+++ Date and Time Module/use of pytz for setting timezone.py
@@ -1,10 +1,14 @@
-import pytz
-from datetime import datetime
-region1="Asia/Karachi"
-region2="Asia/Kolkata"
-Pak_timezone=pytz.timezone(region1)
-India_timezone=pytz.timezone(region2)
-pak_time=datetime.now(Pak_timezone).time()
-india_time=datetime.now(India_timezone).time()
-print(f"pak_time= {pak_time}")
-print(f"india_time= {india_time}")
+import pytz 
+from datetime import datetime  
+
+region1 = "Asia/Karachi"  # Define the time zone region for Karachi
+region2 = "Asia/Kolkata"  # Define the time zone region for Kolkata (India)
+
+Pak_timezone = pytz.timezone(region1)  # Create a time zone object for Karachi
+India_timezone = pytz.timezone(region2)  # Create a time zone object for Kolkata
+
+pak_time = datetime.now(Pak_timezone).time()  # Get the current time in the Karachi time zone
+india_time = datetime.now(India_timezone).time()  # Get the current time in the Kolkata time zone
+
+print(f"pak_time = {pak_time}")  # Print the current time in the Karachi time zone
+print(f"india_time = {india_time}")  # Print the current time in the Kolkata time zone
