53773b9e65450e34cce4a03eab4514af592d67af
Usama-Hanif131 <muhammadosama0121@gmail.com>
2023-08-10 16:03:44 +0500

    adding comments in strftime module program

--- Date and Time Module/use of strftime.py
+++ Date and Time Module/use of strftime.py
@@ -1,5 +1,10 @@
 #converting our date-time object into custom date-time string
 from datetime import datetime as dt
-current_time=dt.now()
-print("Time=", current_time.strftime("%I:%M:%S %p"))
-print("Date=", current_time.strftime("%A %m,%Y"))
+# Get the current date and time
+current_time = dt.now()
+
+# Print the current time in a custom format
+print("Time =", current_time.strftime("%I:%M:%S %p"))  # %I: Hour (12-hour clock), %M: Minute, %S: Second, %p: AM/PM indicator
+
+# Print the current date in a custom format
+print("Date =", current_time.strftime("%A %m, %Y"))  # %A: Full weekday name, %m: Month (numeric), %Y: Year (4 digits)
